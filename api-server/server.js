fc38312ea4fe68691c47124a7beff6e05b878ede
dunixify <vmelkote@ucsc.edu>
2018-06-02 16:46:33 -0700

    query returning individual fields on blocks now

--- api-server/server.js
+++ api-server/server.js
@@ -29,54 +29,87 @@ const config = {
 const eos = Eos.Localnet(config)
 
 //define graphQL query schema
-// const schema = buildSchema(`
-//   type region {
-//     region: Int
-//     cycles_summary: [String]
-//   }
-//   type Block {
-//     previous: String
-//     timestamp: String
-//     transaction_mroot: String
-//     action_mroot: String
-//     block_mroot: String
-//     producer: String
-//     schedule_version: Int
-//     new_producers: [String]
-//     producer_signature: String
-//     regions: [region]
-//     input_transaction: [String]
-//     id: String!
-//     block_num: Int!
-//     ref_block_prefix: Int
-//   }
-//   type Query {
-//     block(numbers: [Int]): Block
-//   }
-// `);
+//numbers like block_num and ref_block_prefix are better as String 
+//since sometimes it is too large to be represented as a 32-bit Int 
+//type by GraphQL
+
 const schema = buildSchema(`
+  type Block {
+    previous: String
+    timestamp: String
+    transaction_mroot: String
+    action_mroot: String
+    block_mroot: String
+    producer: String
+    schedule_version: Float
+    producer_signature: String
+    id: String
+    block_num: Int
+    ref_block_prefix: String
+    new_producers: [String]
+  }
   type Query {
-    block(numbers: [Int]): String
-  }  
-`)
+    block(numbers: [Int]): Block
+  }
+`);
 
 //define root query
 const rootQuery = { 
-  block: getBlockData().then(data => JSON.stringify(data))
+  block: getBlockData()
+        .then(data => 
+          { 
+
+            return {
+              previous: data.previous,
+              timestamp: data.timestamp,
+              transaction_mroot: data.transaction_mroot,
+              action_mroot: data.action_mroot,
+              block_mroot: data.block_mroot,
+              producer: data.producer,
+              schedule_version: data.schedule_version,
+              producer_signature: data.producer_signature,
+              id: data.id,
+              block_num: data.block_num,
+              ref_block_prefix: data.ref_block_prefix,
+              new_producers: data.new_producers
+            } 
+          }
+        )
 };
 
 //function to continuously update the root query every second so it remains
 //the latest block
-setInterval(() => {getBlockData().then(data => rootQuery.block = JSON.stringify(data))}, 1000)
+setInterval(() => 
+  { getBlockData()
+    .then(data => 
+      {
+        rootQuery.block = {
+          previous: data.previous,
+          timestamp: data.timestamp,
+          transaction_mroot: data.transaction_mroot,
+          action_mroot: data.action_mroot,
+          block_mroot: data.block_mroot,
+          producer: data.producer,
+          schedule_version: data.schedule_version,
+          producer_signature: data.producer_signature,
+          id: data.id,
+          block_num: data.block_num,
+          ref_block_prefix: data.ref_block_prefix,
+          new_producers: data.new_producers
+        }
+      }
+    )
+  }
+  , 1000)
 
 //wrap EOS API call to fetch block in try/catch function 
-//to handle promise rejection since it's called multiple times
+//to handle bad block nums since it's called multiple times
 async function fetchBlock(blockNum){
   try{
     return await eos.getBlock(blockNum);
   }
   catch(e){
-    console.log("EOS API error: " + String(e))
+    console.log("EOS API returned an error; check block number")
     return;
   }
 }
@@ -97,6 +130,12 @@ async function getBlockData(blockNum){
 
 //temp tests
 // setInterval(getBlockData().then(data => console.log(data)));
+// getBlockData().then(data => {
+//   console.log(data)
+//   console.log(data.regions)
+//   console.log(data.regions[0].cycles_summary[0][0].transactions)
+//   }
+// )
 
 //api-route for eos block info
 app.use('/block', graphqlHTTP({
